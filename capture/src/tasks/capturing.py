f5fe92c65d14476ba0825a0d428b95cc05de1fd8
Brandon Wiley <brandon@brandon-desktop.(none)>
2013-03-20 15:37:34 -0600

    Added split tasks

---
+++ capture/src/tasks/capturing.py
@@ -0,0 +1,128 @@
+# pavement.py is the config file for paver.
+# It contains all of the paver tasks and default options.
+
+import os
+import sys
+import glob
+import time
+import json
+import shutil
+
+from paver.easy import *
+from paver.path import *
+
+sys.path.append(os.path.abspath('.'))
+
+# Options required by different tasks
+options(
+  # Capture configuration
+  capture=Bunch(
+    traceDir='traces/test',
+    captureDevice='eth1'
+  ),
+  # Capture configuration for HTTPS
+  task=Bunch(
+    capturing=Bunch(
+      capture_https=Bunch(
+        # default trace directory
+        traceDir='traces/test',
+        captureDevice='eth1'
+      )
+    )
+  ),
+  # Capture configuration for obfs2
+  capture_obfsproxy=Bunch(
+    # default trace directory
+    traceDir='traces/test',
+    captureDevice='eth1'
+  ),
+  # Capture configuration for Dust
+  capture_dust=Bunch(
+    # default trace directory
+    traceDir='traces/test',
+    captureDevice='eth1'
+  )
+)
+
+# Capture HTTPS, HTTP-Tor-obfs2, and HTTP-Tor-Dust traffic into traces in the specified directory
+@task
+@cmdopts([
+  ('traceDir=', 'd', 'Directory in which to output traces')
+])
+def capture(options):
+  options.capture_https.traceDir=options.capture.traceDir
+  call_task('capture_https')
+
+  options.capture_obfsproxy.traceDir=options.capture.traceDir
+  call_task('capture_obfsproxy')
+
+  options.capture_dust.traceDir=options.capture.traceDir
+  call_task('capture_dust')
+
+  call_task('process')
+
+# Capture HTTP-Tor-obfs2 traffic into traces in the specified directory
+@task
+@needs(['prepareEncoders'])
+@cmdopts([
+  ('traceDir=', 'd', 'Directory in which to output traces')
+])
+def capture_obfsproxy(options):
+  traceDir=options.capture_obfsproxy.traceDir
+  if not os.path.exists(traceDir):
+    os.mkdir(traceDir)
+
+  sh('touch CAPTURE_RUNNING')
+  sh('sudo src/capture.py eth1 '+str(traceDir)+'/obfsproxy.pcap &')
+
+  call_task('run_obfsproxy')
+
+  call_task('generate_tor_obfs2')
+
+  call_task('kill_obfsproxy')
+
+  sh('rm CAPTURE_RUNNING')
+  time.sleep(11)
+
+# Capture HTTP-Tor-Dust traffic into traces in the specified directory
+@task
+@needs(['prepareEncoders'])
+@cmdopts([
+  ('traceDir=', 'd', 'Directory in which to output traces')
+])
+def capture_dust(options):
+  traceDir=options.capture_dust.traceDir
+  if not os.path.exists(traceDir):
+    os.mkdir(traceDir)
+
+  sh('touch CAPTURE_RUNNING')
+  sh('sudo src/capture.py wlan0 '+str(traceDir)+'/dust.pcap &')
+
+  call_task('run_dust')
+
+  call_task('generate_http_dust')
+
+  call_task('kill_dust')
+
+  sh('rm CAPTURE_RUNNING')
+  time.sleep(11)
+
+# Capture HTTPS traffic into traces in the specified directory
+@task
+@cmdopts([
+  ('traceDir=', 'd', 'Directory in which to output traces'),
+  ('captureDevice=', 'c', 'Device to use for capturing traces')
+])
+def capture_https(options):
+  traceDir=options.capture_https.traceDir
+  if not os.path.exists(traceDir):
+    os.mkdir(traceDir)
+  captureDevice=options.capture_https.captureDevice
+
+  sh('touch CAPTURE_RUNNING')
+  sh('sudo src/capture.py '+captureDevice+' '+str(traceDir)+'/https.pcap &')
+
+  call_task('generate_https')
+
+  sh('rm CAPTURE_RUNNING')
+  time.sleep(11)
