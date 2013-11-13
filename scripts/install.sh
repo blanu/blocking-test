a2b599b6f7977b3cf1ff87ba1611bfa039000ac4
Brandon Wiley <github@blanu.net>
2013-11-13 17:53:37 -0600

    install.sh now assumes a previous git checkout

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,12 +1,11 @@
-apt-get install git-core
-apt-get install python-pip
-apt-get install traceroute
-apt-get install nmap
-apt-get install python-nose
-apt-get install python-scapy
-pip install paver
-pip install webdriverplus
-git clone https://github.com/blanu/blocking-test
-cd blocking-test/
-cp scripts/Run ~/Desktop
-cp scripts/Update ~/Desktop
+sudo apt-get install python-pip
+sudo apt-get install traceroute
+sudo apt-get install nmap
+sudo apt-get install python-nose
+sudo apt-get install python-scapy
+sudo pip install paver
+sudo pip install webdriverplus
+
+cd ~/blocking-test/
+sudo cp scripts/Run /usr/share/applications
+cp scripts/Update /usr/share/applications
