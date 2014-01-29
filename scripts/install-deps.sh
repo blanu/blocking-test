f41a3c363442d7181e1de7ac51c0be4f574df7cd
BT2 <bt2@bt2-0002.(none)>
2014-01-29 14:08:22 -0500

    Install Dust and Dust-tools from source instead of packages

--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -7,6 +7,22 @@ sudo apt-get install firefox
 sudo apt-get install haskell-platform
 sudo pip install paver
 sudo pip install webdriverplus
-scripts/install-threefish.sh
-cabal install Dust
-cabal install Dust-tools
+sudo apt-get install libssl-dev libssl1.0.0
+#scripts/install-threefish.sh
+cabal install threefish
+pushd ~
+git clone https://github.com/blanu/Dust
+cd Dust
+git pull origin master
+cd hs/Dust-crypto
+cabal install
+cd ..
+cabal install
+popd
+
+pushd ~
+git clone https://github.com/blanu/Dust-tools
+cd Dust-tools
+git pull origin master
+cabal install
+popd
