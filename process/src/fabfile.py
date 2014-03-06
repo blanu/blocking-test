e0626326bcb0a35f0304a073e695a174169a9b1f
Brandon Wiley <brandon@blanu.net>
2014-03-06 11:03:45 -0600

    cd to blocking-test

--- process/src/fabfile.py
+++ process/src/fabfile.py
@@ -10,61 +10,6 @@ import os
 
 from fabric.api import cd, run
 
-# Bootstrap by installing blocking-test source, updating to latest version, and running bootstrap.py
-#def bootstrap():
-#  if run('test -d blocking-test').failed:
-#    run('git clone https://gitweb.torproject.org/user/blanu/blocking-test.git')
-#  with cd('blocking-test'):
-#    run('git pull origin master')
-#    if run('test -d bin').failed:
-#      run('python bootstrap.py')
-
-# Install dependencies: libevent, tor, and obfs2
-def prepare():
-  with cd('blocking-test'):
-    run('source bin/activate')
-    run('paver libevent')
-    run('paver tor')
-    run('paver obfsproxy')
-
-# Run obfs2 server
-def run_obfsproxy_server():
-  with cd('blocking-test'):
-    run('paver run_local_obfsproxy_server')
-
-# Kill obfs2 server
-def kill_obfsproxy():
-  with cd('blocking-test'):
-    run('paver kill_local_obfsproxy')
-
-# Run Dust server
-def run_dust_server():
-  with cd('blocking-test'):
-    run('paver run_local_dust_server')
-
-# Kill Dust server
-def kill_dust():
-  with cd('blocking-test'):
-    run('paver kill_local_dust')
-
-# Run Tor bridge
-def run_tor_bridge():
-  with cd('blocking-test'):
-    run('paver run_local_tor_bridge')
-
-# Kill Tor bridge
-def kill_tor():
-  with cd('blocking-test'):
-    run('paver kill_local_tor')
-
-def run_dust_replay_http_server():
-  with cd('blocking-test/capture'):
-    run('paver run_local_dust_replay_http_server')
-
-# Kill Dust server
-def kill_dust_replay_http_server():
-  with cd('blocking-test/capture'):
-    run('paver kill_local_dust_replay_http_server')
-
 def list():
-  run('ls *.zip')
+  with cd('blockint-test'):
+    run('ls *.zip')
