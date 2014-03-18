2e942688c1252b8db8603307bd6af027c6bb13a0
Brandon Wiley <brandon@blanu.net>
2014-03-18 17:06:44 -0500

    Configure SSH keys and config on install

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,4 +1,20 @@
-scripts/install-deps.sh
+if [ ! -e "$HOME/.ssh/id_rsa.pub" ]; then
+    echo "Generating SSH key"
+    ssh-keygen
+fi
 
-sudo cp scripts/Run /usr/share/applications
-sudo cp scripts/Update /usr/share/applications
+if [ ! -e "/root/.ssh/id_rsa.pub" ]; then
+  if [ -d "/root" ]; then
+    echo "Copying user SSH key for root"
+    cp ~/.ssh/id_rsa* /root/.ssh
+    chown root /root/.ssh/id_rsa*
+    chgrp root /root/.ssh/id_rsa*
+  fi
+fi
+
+if [ ! -e "$HOME/.ssh/config" ]; then
+    echo "Copying SSH config"
+    cp ~/blocking-test/capture/config/ssh-config ~/.ssh/config
+fi
+
+#scripts/install-deps.sh
